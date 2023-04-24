c86ae2d0f7c3c548091253058cb14903ca9dcde0
DrewnioQ <debski.dev@gmail.com>
2023-04-24 18:56:23 +0200

    Add font fallbacks for added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ export default {
         screen: "100svh",
       },
       fontFamily: {
-        sans: "Inter, sans-serif",
-        lato: "Lato, sans-serif",
-        poppins: "Poppins, sans-serif",
+        sans: "Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+        lato: "Lato, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+        poppins:
+          "Poppins, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
       },
     },
   },
